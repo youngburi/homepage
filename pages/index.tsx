0a601b732ec0e3a20044c53705a932897fdd9bc4
Young Buri <32440380+youngburi@users.noreply.github.com>
2021-01-31 19:49:41 +0100

    added textbox, title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,88 @@
-import { Box } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const IndexPage = () => (
   <Box
-    height="100vh"
+    minHeight="100vh"
     bg={`
     radial-gradient(circle at 40% -20%, rgba(216, 235, 52, 0.4), transparent 30%),
     radial-gradient(circle at 25% 120%, rgba(216, 235, 52, 0.2), transparent 30%),
     radial-gradient(circle at 60% 70%, rgba(230, 230, 230, 0.2), transparent 30%),
     radial-gradient(circle at -50% 100%, rgba(20, 60, 60, 0.8), transparent 70%),
     radial-gradient(circle at 110% 50%, rgba(20, 60, 60, 0.2), transparent 50%),
-    linear-gradient(
-      90deg, 
-      rgba(131,58,180,0.3) 0%, 
-      rgba(253,29,29,0.2) 50%, 
-      rgba(252,176,69,0.2) 100%
-      ),
-      linear-gradient(0deg, rgba(255, 30, 40, 0.2), rgba(255, 255, 255, 0.8)), 
-      linear-gradient(90deg, rgba(20, 255, 200, 0.5), rgba(100, 50, 100, 0.5)),
-      radial-gradient(circle, rgba(238,174,202,0.5) 0%, rgba(148,187,233,0.6) 100%)
-  `}
-    // css={css`blur(1px);`}
+    linear-gradient(90deg, rgba(131,58,180,0.3) 0%, rgba(253,29,29,0.2) 50%, rgba(252,176,69,0.2) 100%),
+    linear-gradient(0deg, rgba(255, 30, 40, 0.2), rgba(255, 255, 255, 0.8)), 
+    linear-gradient(90deg, rgba(20, 255, 200, 0.5), rgba(100, 50, 100, 0.5)),
+    // radial-gradient(circle at 100% 100%, transparent, transparent 55%, rgba(255,255,255, 0.4) 65%, transparent 70%),
+    radial-gradient(ellipse at -110% 110%, transparent, transparent 55%, rgba(255,255,255, 0.4) 65%, transparent 68%),
+    radial-gradient(circle, rgba(238,174,202,0.5) 0%, rgba(148,187,233,0.6) 100%)
+    `}
+    // css={css`
+    //   filter: blur(1px);
+    // `}
+    p="8"
   >
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <Heading as="h1" fontSize="5rem" mb="4">
+      Buri
+    </Heading>
+    <Box p="8" bg="custom.lightbg" boxShadow="lg">
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+        tenetur, ipsam vitae consequatur ipsum modi voluptates magni nam ullam
+        earum error fugiat quidem mollitia. Exercitationem, doloremque harum
+        distinctio dignissimos natus architecto quia maiores! Nobis mollitia
+        dolores unde nisi sed debitis eum laudantium rerum voluptatem,
+        consequatur iure neque iste rem itaque ipsum nostrum exercitationem id.
+        Earum ipsam esse veniam consectetur unde ipsum et ducimus laborum eius.
+        Facere, perspiciatis rerum. Voluptas ea placeat, reiciendis unde vitae
+        ipsum, fuga cumque corporis odit atque cupiditate sed eaque consequatur
+        quaerat laboriosam praesentium debitis ullam fugiat magni quod tenetur
+        neque quos! Eum beatae provident aut distinctio dolorum vel dignissimos
+        voluptate quam, deserunt, dicta debitis. Assumenda vel quo nisi autem
+        modi accusantium non animi ducimus atque. Alias commodi praesentium sed,
+        possimus culpa recusandae excepturi fuga eveniet quisquam ipsa
+        exercitationem ducimus pariatur, optio mollitia. Adipisci consequatur
+        maiores et earum modi perspiciatis sequi ex excepturi! Ab, libero
+        accusamus dicta odit ratione, harum commodi itaque aut odio aliquid
+        molestiae, voluptate veritatis. In laboriosam minima voluptatum pariatur
+        nostrum reprehenderit quas ab error dicta numquam aspernatur possimus
+        doloremque, quibusdam a temporibus animi mollitia est quis rerum tenetur
+        eveniet molestias alias quaerat maxime. Voluptatem facilis iste quia et
+        recusandae ea libero vero optio incidunt tempora odit, eos deleniti
+        officia praesentium nostrum est odio sint dolore obcaecati quaerat
+        laborum. Rem ipsam ipsa, eos necessitatibus similique labore at tempora
+        atque eum omnis officia nam architecto voluptates distinctio aperiam
+        velit esse quaerat tempore! Error accusantium ratione impedit, deserunt
+        quia ipsum! Quis unde eveniet fuga vitae! Corporis consequatur nulla
+        officiis voluptatem id modi totam cupiditate, temporibus deleniti quas
+        laborum! Vel, reiciendis velit? Libero quo doloribus id quod. Culpa
+        aliquam possimus assumenda asperiores explicabo autem perferendis
+        provident ipsa, consequuntur praesentium, tenetur quasi minima accusamus
+        voluptas laboriosam quo iusto recusandae sit animi adipisci consequatur
+        corrupti. Cumque error, nemo in provident voluptatem reiciendis
+        dignissimos autem velit vel earum obcaecati, porro ipsam itaque! Vitae
+        voluptas possimus dicta sequi repellendus suscipit expedita pariatur
+        sapiente recusandae iure dolorem asperiores, beatae odit quod
+        consequuntur quasi cupiditate esse optio, exercitationem quia amet ipsum
+        voluptatibus. At, fuga. Animi iste deleniti facere incidunt aspernatur
+        soluta dicta tempore similique? Saepe eius voluptates, tenetur totam
+        provident nostrum. Corporis inventore odio dolore molestias ipsum id,
+        cumque sequi rem optio suscipit quos quam ducimus quia illum ipsa saepe
+        sit consequuntur! Aliquam laudantium in repudiandae nam quae, minus,
+        numquam commodi fugit quisquam fugiat obcaecati libero optio animi
+        rerum, adipisci accusamus doloribus dolor est itaque architecto
+        doloremque ipsam! Quas quod officiis optio possimus reiciendis repellat
+        recusandae reprehenderit molestiae facere numquam accusantium sunt
+        veritatis facilis labore corporis aliquid dolores iste exercitationem,
+        quam atque tempora fugiat odio ea! Quod non amet atque vero officia
+        excepturi quis minus dicta minima repudiandae, vel accusamus dignissimos
+        debitis aspernatur hic, beatae maxime ducimus cum, modi iusto dolore
+        molestias iure. Ratione eligendi soluta atque obcaecati quod, fuga earum
+        nam enim ipsum corporis. Adipisci vel consequatur molestias earum neque
+        dolorem magni eaque voluptates libero cum maxime omnis officiis, eos
+        architecto unde eum modi et culpa. Quidem minus iusto deserunt facilis
+        reprehenderit?
+      </Text>
+    </Box>
   </Box>
 );
 
