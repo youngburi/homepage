a9f036cc8bd091ab82c6fb086413a979d033f4ef
Young Buri <32440380+youngburi@users.noreply.github.com>
2021-02-09 18:09:41 +0100

    added contentboxes

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,23 @@ const IndexPage = () => {
         gridTemplateRows="auto 1fr"
       >
         <Flex alignItems="center">
+          <Heading
+            mr={["3", "3", "6"]}
+            color="white"
+            as="h1"
+            fontSize={["5rem", "5rem", "clamp(1rem, 20vw, 6rem)"]}
+            lineHeight={["3rem", "3rem", "clamp(1rem, 20vw, 6rem)"]}
+            mb={["-8px", "-8px", "clamp(-10px, -2vh, -20px)"]}
+            transition="300ms ease"
+            // zIndex="5"
+            // fontFamily="singleDay"
+            // fontWeight="normal"
+          >
+            Buri
+          </Heading>
           <SquareBox
             color="transparent"
-            size={["4rem", "4rem", "clamp(1rem, 14vw, 6rem)"]}
+            size={["4rem", "4rem", "clamp(1rem, 14vw, 5rem)"]}
             borderRadius={["50%", "50%", "50%"]}
           >
             <Image
@@ -30,20 +44,6 @@ const IndexPage = () => {
               transition="300ms ease"
             />
           </SquareBox>
-          <Heading
-            ml={["3", "3", "6"]}
-            color="white"
-            as="h1"
-            fontSize={["5rem", "5rem", "clamp(1rem, 20vw, 6rem)"]}
-            lineHeight={["3rem", "3rem", "clamp(1rem, 20vw, 6rem)"]}
-            mb={["-8px", "-8px", "clamp(-10px, -2vh, -20px)"]}
-            transition="300ms ease"
-            zIndex="5"
-            // fontFamily="singleDay"
-            // fontWeight="normal"
-          >
-            Buri
-          </Heading>
         </Flex>
         <Box display="grid" placeItems="center" height="100%">
           <Text
@@ -62,6 +62,60 @@ const IndexPage = () => {
               <br />
             </Text>
             painter and mUsician also by daY
+          </Text>
+        </Box>
+      </Box>
+      <Box p={["4", "4", "6"]} display="flex" justifyContent="flex-end">
+        <Box
+          bg="linear-gradient(145deg, rgba(255, 154, 204, 0.5), rgba(255, 236, 255, 0.3))"
+          position="relative"
+          borderRadius={["1rem", "1rem", "2rem"]}
+          p={["4", "4", "8"]}
+          width={["100%", "80%", "60%"]}
+          // boxShadow="10px 19px 38px rgba(255, 154, 204, 0.5), -19px -10px 38px rgba(255, 236, 255, 0.3)"
+          style={{ backdropFilter: "blur(5px)" }}
+        >
+          <Text fontSize="4rem" fontWeight="600" color="white">
+            Paintings
+          </Text>
+          <Text
+            fontSize={["1.4rem", "1.5rem", "1.6rem"]}
+            fontWeight="300"
+            color="white"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+            error enim, officiis dolores totam architecto molestiae a veniam
+            expedita nemo libero aliquam ullam nostrum dolor laborum magnam
+            perferendis? Eius voluptatum omnis nobis. Praesentium omnis quisquam
+            necessitatibus doloremque magni laboriosam sequi nisi voluptates
+            molestiae assumenda vero id quidem, et explicabo voluptatem?
+          </Text>
+        </Box>
+      </Box>
+      <Box p={["4", "4", "6"]} display="flex" justifyContent="flex-start">
+        <Box
+          bg="linear-gradient(145deg, rgba(161, 225, 255, 0.5), rgba(161, 255, 221, 0.3))"
+          position="relative"
+          borderRadius={["1rem", "1rem", "2rem"]}
+          p={["4", "4", "8"]}
+          width={["100%", "85%", "clamp(40rem,80%,60rem)"]}
+          // boxShadow="10px 19px 38px rgba(161, 255, 197, 0.5), -19px -10px 38px rgba(255, 236, 255, 0.3)"
+          style={{ backdropFilter: "blur(5px)" }}
+        >
+          <Text fontSize="4rem" fontWeight="600" color="white">
+            Music
+          </Text>
+          <Text
+            fontSize={["1.4rem", "1.5rem", "1.6rem"]}
+            fontWeight="300"
+            color="white"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+            error enim, officiis dolores totam architecto molestiae a veniam
+            expedita nemo libero aliquam ullam nostrum dolor laborum magnam
+            perferendis? Eius voluptatum omnis nobis. Praesentium omnis quisquam
+            necessitatibus doloremque magni laboriosam sequi nisi voluptates
+            molestiae assumenda vero id quidem, et explicabo voluptatem?
           </Text>
         </Box>
       </Box>
