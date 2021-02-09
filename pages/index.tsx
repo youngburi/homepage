4a248230a402ef71ef99f228873b5473f8356e4a
Young Buri <32440380+youngburi@users.noreply.github.com>
2021-02-09 23:56:39 +0100

    new black box, removed box in title

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,12 @@ const IndexPage = () => {
       >
         <Flex
           alignItems="center"
-          boxShadow="0 4px 6px -1px rgb(150 150 150 / 10%), 0 2px 4px -1px rgb(150 150 150 / 6%)"
-          style={{ backdropFilter: "blur(5px) brightness(1)" }}
-          bg="linear-gradient(145deg, rgba(255, 154, 204, 0.5), rgba(255, 236, 255, 0.3))"
-          borderRadius={["1rem", "1rem", "2rem"]}
-          p={["4", "4", "6"]}
-          width="fit-content"
+          // boxShadow="0 4px 6px -1px rgb(150 150 150 / 10%), 0 2px 4px -1px rgb(150 150 150 / 6%)"
+          // style={{ backdropFilter: "blur(5px) brightness(1)" }}
+          // bg="linear-gradient(145deg, rgba(255, 154, 204, 0.5), rgba(255, 236, 255, 0.3))"
+          // borderRadius={["1rem", "1rem", "2rem"]}
+          // p={["4", "4", "6"]}
+          // width="fit-content"
         >
           <Heading
             mr={["3", "3", "6"]}
@@ -111,6 +111,35 @@ const IndexPage = () => {
       <Box p={["4", "4", "6"]} display="flex" justifyContent="flex-start">
         <Box
           bg="linear-gradient(145deg, rgba(161, 225, 255, 0.5), rgba(161, 255, 221, 0.3))"
+          position="relative"
+          borderRadius={["1rem", "1rem", "2rem"]}
+          p={["4", "4", "8"]}
+          width={["100%", "85%", "clamp(40rem,80%,60rem)"]}
+          // boxShadow="10px 19px 38px rgba(161, 255, 197, 0.5), -19px -10px 38px rgba(255, 236, 255, 0.3)"
+          boxShadow="0 4px 6px -1px rgb(150 150 150 / 10%), 0 2px 4px -1px rgb(150 150 150 / 6%)"
+          style={{ backdropFilter: "blur(5px) brightness(0.9)" }}
+        >
+          <Text fontSize="4rem" fontWeight="600" color="rgba(248,250,255, 0.8)">
+            Music
+          </Text>
+          <Text
+            fontSize={["1.4rem", "1.5rem", "1.6rem"]}
+            // fontWeight="300"
+            color="rgba(248,250,255, 1)"
+            textAlign={["justify", "justify", "left"]}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+            error enim, officiis dolores totam architecto molestiae a veniam
+            expedita nemo libero aliquam ullam nostrum dolor laborum magnam
+            perferendis? Eius voluptatum omnis nobis. Praesentium omnis quisquam
+            necessitatibus doloremque magni laboriosam sequi nisi voluptates
+            molestiae assumenda vero id quidem, et explicabo voluptatem?
+          </Text>
+        </Box>
+      </Box>
+      <Box p={["4", "4", "6"]} display="flex" justifyContent="flex-start">
+        <Box
+          bg="black"
           position="relative"
           borderRadius={["1rem", "1rem", "2rem"]}
           p={["4", "4", "8"]}
