6cef7cdbfb93963521dfe7a69bc7f368712875c7
Young Buri <32440380+youngburi@users.noreply.github.com>
2021-03-16 00:53:10 +0100

    added texts below

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import { backgroundAnimation, gradientBackground } from "../utils/styles";
 
@@ -17,7 +17,7 @@ const IndexPage = () => {
           >
             <Text
               color="white"
-              transition="300ms ease"
+              transition="font-size 300ms ease"
               fontSize={["clamp(1rem, 3vw, 2rem)"]}
               textAlign="center"
             >
@@ -29,6 +29,49 @@ const IndexPage = () => {
             </Text>
           </Box>
         </Box>
+      </Box>
+      <Box px={["2rem", "2rem"]} pb={["2rem", "2rem"]}>
+        <Heading
+          as="h1"
+          fontSize={["2rem", "3rem", "4rem"]}
+          transition="font-size 300ms ease"
+        >
+          Music
+        </Heading>
+        <Text textAlign="justify">
+          I have been making music – on and off – for over 15 years. Lorem
+          ipsum, dolor sit amet consectetur adipisicing elit. Praesentium nisi
+          cupiditate assumenda maiores odit inventore quos, corrupti non
+          repellat culpa perspiciatis at atque sequi ea molestiae sint! Unde
+          aspernatur quaerat aliquid itaque ab architecto nulla ea perspiciatis
+          qui sit cumque doloremque earum aliquam dolor, recusandae iusto omnis
+          necessitatibus, culpa dolorem enim. Iusto expedita odio ratione libero
+          fugit minus numquam cum deserunt dolore, architecto laudantium
+          provident repellendus mollitia officiis ipsa voluptates? A laboriosam
+          molestiae possimus fugiat eum eius eaque maxime accusamus voluptates
+          distinctio perferendis, blanditiis, explicabo cum doloremque nesciunt
+          officiis, totam quasi omnis saepe tempore tempora nam dicta. Quod,
+          dolores est.
+        </Text>
+        <Heading
+          as="h1"
+          fontSize={["2rem", "3rem", "4rem"]}
+          transition="font-size 300ms ease"
+          pt={["2rem", "2rem"]}
+        >
+          Painting
+        </Heading>
+        <Text textAlign="justify">
+          Apparently as many others, I have picked up the brush and started
+          painting with oil during the lockdown. Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Enim excepturi perferendis nisi dolorem
+          voluptatem a quidem vero consequatur earum! Inventore excepturi,
+          explicabo, quas consequuntur in optio placeat cupiditate iusto
+          ratione, possimus earum quo adipisci veritatis sed deserunt corporis
+          itaque perspiciatis nostrum dolorem sunt autem illo laudantium
+          consequatur! A eligendi, odit architecto omnis obcaecati porro. Nam ab
+          atque ratione sapiente voluptates!
+        </Text>
       </Box>
     </Box>
   );
